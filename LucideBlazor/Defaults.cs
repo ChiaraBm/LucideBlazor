0deb96a3ef266cce77e7b84805dba011c142d713
Bubbies <66354328+bub-bl@users.noreply.github.com>
2026-01-04 14:50:55 +0100

    DefaultAttributes can be readonly

--- LucideBlazor/Defaults.cs
+++ LucideBlazor/Defaults.cs
@@ -4,7 +4,7 @@ namespace LucideBlazor;
 
 internal static class Defaults
 {
-    internal static FrozenDictionary<string, object> DefaultAttributes = new Dictionary<string, object>()
+    internal static readonly FrozenDictionary<string, object> DefaultAttributes = new Dictionary<string, object>
     {
         ["xmlns"] = "http://www.w3.org/2000/svg",
         ["width"] = 24,
