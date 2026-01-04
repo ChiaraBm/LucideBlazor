990b40ec90dde2bcd31a77b9d2dac0ad6db0278d
Bubbies <66354328+bub-bl@users.noreply.github.com>
2026-01-04 15:34:36 +0100

    Support typed properties for LucideIcon and generated icon classes

--- LucideBlazor/IconBase.cs
+++ LucideBlazor/IconBase.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using LucideBlazor.Extensions;
+using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Rendering;
 
 namespace LucideBlazor;
@@ -6,15 +7,21 @@ namespace LucideBlazor;
 public abstract class IconBase : IComponent
 {
     [Parameter] public string? ClassName { get; set; }
+    [Parameter] public int Size { get; set; } = 24;
+    [Parameter] public string Fill { get; set; } = "none";
+    [Parameter] public string Stroke { get; set; } = "currentColor";
+    [Parameter] public double StrokeWidth { get; set; } = 2;
+    [Parameter] public StrokeLineCap StrokeLineCap { get; set; } = StrokeLineCap.Round;
+    [Parameter] public StrokeLineJoin StrokeLineJoin { get; set; } = StrokeLineJoin.Round;
 
     [Parameter(CaptureUnmatchedValues = true)]
-    public Dictionary<string, object?>? Props { get; set; }
-    
-    private RenderHandle Handle;
+    public Dictionary<string, object>? Props { get; set; }
+
+    private RenderHandle _handle;
 
     public void Attach(RenderHandle renderHandle)
     {
-        Handle = renderHandle;
+        _handle = renderHandle;
     }
 
     protected void RenderIcon(RenderTreeBuilder builder, string svgContent)
@@ -22,25 +29,32 @@ public abstract class IconBase : IComponent
         var counter = 0;
 
         builder.OpenElement(0, "svg");
-
         builder.AddAttribute(counter++, "class", ClassName);
 
-        if (Props != null)
-        {
-            foreach (var attribute in Defaults.DefaultAttributes)
-            {
-                if (!Props.ContainsKey(attribute.Key))
-                    builder.AddAttribute(counter++, attribute.Key, attribute.Value);
-            }
+        AddIfNotInProps("xmlns", "http://www.w3.org/2000/svg");
+        AddIfNotInProps("width", Size);
+        AddIfNotInProps("height", Size);
+        AddIfNotInProps("viewBox", "0 0 24 24");
+        AddIfNotInProps("fill", Fill);
+        AddIfNotInProps("stroke", Stroke);
+        AddIfNotInProps("stroke-width", StrokeWidth);
+        AddIfNotInProps("stroke-linecap", StrokeLineCap.ToKebabCase());
+        AddIfNotInProps("stroke-linejoin", StrokeLineJoin.ToKebabCase());
 
+        if (Props is not null)
             builder.AddMultipleAttributes(counter++, Props);
-        }
-        else
-            builder.AddMultipleAttributes(counter++, Defaults.DefaultAttributes);
 
-        builder.AddMarkupContent(counter++, svgContent);
-
+        builder.AddMarkupContent(counter, svgContent);
         builder.CloseElement();
+
+        return;
+
+        // Helper to add attribute if not present in Props
+        void AddIfNotInProps(string name, object value)
+        {
+            if (Props is null || !Props.ContainsKey(name))
+                builder.AddAttribute(counter++, name, value);
+        }
     }
 
     protected abstract void HandleRender(RenderTreeBuilder builder);
@@ -52,19 +66,35 @@ public abstract class IconBase : IComponent
             switch (parameter.Name)
             {
                 case nameof(ClassName):
-                    ClassName = (string)parameter.Value;
+                    ClassName = (string?)parameter.Value;
                     break;
-                
+                case nameof(Size):
+                    Size = (int)parameter.Value;
+                    break;
+                case nameof(Fill):
+                    Fill = (string)parameter.Value;
+                    break;
+                case nameof(Stroke):
+                    Stroke = (string)parameter.Value;
+                    break;
+                case nameof(StrokeWidth):
+                    StrokeWidth = parameter.Value is int i ? i : (double)parameter.Value;
+                    break;
+                case nameof(StrokeLineCap):
+                    StrokeLineCap = (StrokeLineCap)parameter.Value;
+                    break;
+                case nameof(StrokeLineJoin):
+                    StrokeLineJoin = (StrokeLineJoin)parameter.Value;
+                    break;
                 default:
-                    Props ??= new Dictionary<string, object?>();
+                    Props ??= new Dictionary<string, object>();
                     Props[parameter.Name] = parameter.Value;
-                    
+
                     break;
             }
         }
-        
-        Handle.Render(HandleRender);
 
+        _handle.Render(HandleRender);
         return Task.CompletedTask;
     }
 }
---
+++ LucideBlazor/LucideIcon.cs
@@ -0,0 +1,45 @@
+using LucideBlazor.Extensions;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+
+namespace LucideBlazor;
+
+public class LucideIcon : ComponentBase
+{
+    [Parameter] public string Name { get; set; } = string.Empty;
+    [Parameter] public string? ClassName { get; set; }
+    [Parameter] public int Size { get; set; } = 24;
+    [Parameter] public string Fill { get; set; } = "none";
+    [Parameter] public string Stroke { get; set; } = "currentColor";
+    [Parameter] public double StrokeWidth { get; set; } = 2;
+    [Parameter] public StrokeLineCap StrokeLineCap { get; set; } = StrokeLineCap.Round;
+    [Parameter] public StrokeLineJoin StrokeLineJoin { get; set; } = StrokeLineJoin.Round;
+
+    [Parameter(CaptureUnmatchedValues = true)]
+    public Dictionary<string, object>? Props { get; set; }
+
+    protected override void BuildRenderTree(RenderTreeBuilder builder)
+    {
+        if (string.IsNullOrEmpty(Name)) return;
+
+        var iconType = IconMap.GetIconType(Name);
+        if (iconType is null) return;
+
+        builder.OpenComponent(0, iconType);
+
+        if (ClassName is not null)
+            builder.AddAttribute(1, nameof(IconBase.ClassName), ClassName);
+
+        builder.AddAttribute(2, nameof(IconBase.Size), Size);
+        builder.AddAttribute(3, nameof(IconBase.Fill), Fill);
+        builder.AddAttribute(4, nameof(IconBase.Stroke), Stroke);
+        builder.AddAttribute(5, nameof(IconBase.StrokeWidth), StrokeWidth);
+        builder.AddAttribute(6, nameof(IconBase.StrokeLineCap), StrokeLineCap.ToKebabCase());
+        builder.AddAttribute(7, nameof(IconBase.StrokeLineJoin), StrokeLineJoin.ToKebabCase());
+
+        if (Props is not null)
+            builder.AddMultipleAttributes(8, Props);
+
+        builder.CloseComponent();
+    }
+}
