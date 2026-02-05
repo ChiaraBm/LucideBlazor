7788a771768470a8d4f1b3a0a7313f71eb0dc089
ChiaraBm <chiara.bm04@gmail.com>
2026-02-05 13:55:09 +0100

    Added xml docs. Updated readme files

--- LucideBlazor/LucideIcon.cs
+++ LucideBlazor/LucideIcon.cs
@@ -2,10 +2,17 @@ using Microsoft.AspNetCore.Components;
 
 namespace LucideBlazor;
 
+/// <summary>
+/// Dynamic component for rendering an icon by its name
+/// </summary>
 public class LucideIcon : IconBase
 {
-    [Parameter]
+    /// <summary>
+    /// Name of the icon which should be rendered
+    /// </summary>
+    [Parameter, EditorRequired]
     public string Name { get; set; } = string.Empty;
 
+    /// <inheritdoc/>
     protected override string SvgContent => IconMap.GetIconSvg(Name) ?? string.Empty;
 }
--- LucideBlazor/StrokeLineCap.cs
+++ LucideBlazor/StrokeLineCap.cs
@@ -1,5 +1,8 @@
 namespace LucideBlazor;
 
+/// <summary>
+/// Specifies how an ending of a line should be rendered.
+/// </summary>
 public enum StrokeLineCap
 {
     Butt,
